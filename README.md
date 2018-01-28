7e8786f21306ca24bd1c874a12d8a7c154126839
Marceia Egler <marceiaegler@g.harvard.edu>
2018-01-28 08:08:22 -0500

    updated README

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
 # PuppyTime
 Live Video with Shelter Pets
 
-An app that allows you to video chat with shelter pets and give them treats.  This socializes dogs who are scared in shelter kennels, gives them better exposure to the public, and raises their chances for adoption.  The application also provides users a way to donate to participating shelters and submit adoption applications.
+##Inspiration
+3 million dogs enter shelters nationwide each year. However, less than half are adopted into new homes. Some shelter dogs may receive human interaction 3 times a day for walks, but are then left by themselves.
+
+PuppyTime changes that!
+
+Talking and interacting with shelter dogs improves their wellbeing and makes them more adoptable. We wanted to provide a new way to benefit shelter dogs and bring joy to the humans that chat with them.
+
+##What it does
+PuppyTime is a mobile website that enables folks to live video chat with shelter dogs and give them treats. This gives the dog more social time and builds a personal connection between the user and the dog to increase donations and adoption.
+
+##How we built it
+We built the PuppyTime mobile website with a modified bootstrap template, javascript live video, and PayPayl and PetFinder RESTful APIs. The “give a treat” button calls a RESTful service that runs a treat dispensing routine on the Arduino 101 with Grove Shield and stepper motor (written in C).
+
+##Challenges we ran into
+Software to work with Arduino hardware
+Cable shorts when connecting Arduino components
+Learning new languages/tools
+
+##Accomplishments that we're proud of:
+-Our product works! (tested with a real shelter dog at Houndhaven in Florida)
+-Working well and building each other up as a team 
+-An inclusive and informative experience for our 3 first time hackathon participants
+
+##What we learned
+-How to use an Arduino 101 board 
+-New languages and tools: HTML/CSS, jQuery, new APIs 
+-Collaborative and fun ideation techniques
+
+##What's next for PuppyTime
+-Hook website button to Arduino board to dispense treats
+-Match users with a dog at a shelter near their location so they can choose to adopt them or donate to the local shelter
+-Design fun video filters as a paid feature to benefit local shelters
+-Incorporate Amazon Marketplace to send donations like dog food, treats, toys, etc.
+-Allow users to save their favorite furry friends and chat with them again
--- js/adopt.js
+++ js/adopt.js
@@ -1,40 +1,42 @@
-$(document).ready(function(){
-  console.log("ready!");
+$(document).ready(function() {
+    console.log("ready!");
 
-  var url = 'http://api.petfinder.com/pet.getRandom';
-  
-  $.ajax({
-    url: url,
-    jsonp: "callback",
-    dataType: "jsonp",
-    data: {
-      key: "4a9698752f4139c768c657d4776e1fbf",
-      animal: 'dog',
-      output: 'basic',
-      format: 'json'
-    },
-    // Here is where we handle the response we got back from Petfinder
-    success: function( response ) {
-      console.log(response); // debugging
-      var dogName = response.petfinder.pet.name.$t;
-      var img = response.petfinder.pet.media.photos.photo[2].$t;
-      var id = response.petfinder.pet.id.$t;
+    var url = 'https://api.petfinder.com/pet.getRandom';
 
-      var newName = document.createElement('a');
-      var newDiv = document.createElement('div');
-      newName.textContent = dogName;
-      newName.href = 'https://www.petfinder.com/petdetail/' + id;
+    $.ajax({
+        url: url,
+        jsonp: "callback",
+        dataType: "jsonp",
+        data: {
+            key: "4a9698752f4139c768c657d4776e1fbf",
+            animal: 'dog',
+            output: 'basic',
+            format: 'json'
+        },
+        // Here is where we handle the response we got back from Petfinder
+        success: function(response) {
+            console.log(response); // debugging
 
-      var newImg = document.createElement('img');
-      newImg.src = img;
-      
-      var list = document.createElement("div");
-      list.setAttribute("id", "List");
-      document.body.appendChild(list);
+            var dogName = response.petfinder.pet.name.$t;
+            var img = response.petfinder.pet.media.photos.photo[2].$t;
+            var id = response.petfinder.pet.id.$t;
+            
 
-      newDiv.appendChild(newName);
-      list.appendChild(newDiv);
-      list.appendChild(newImg);
-    }
-  });
-  })
+            var newName = document.createElement('a');
+            var newDiv = document.createElement('div');
+            newName.textContent = dogName;
+            newName.href = 'https://www.petfinder.com/petdetail/' + id;
+
+            var newImg = document.createElement('img');
+            newImg.src = img;
+
+            var list = document.createElement("div");
+            list.setAttribute("id", "List");
+            document.body.appendChild(list);
+
+            newDiv.appendChild(newName);
+            list.appendChild(newDiv);
+            list.appendChild(newImg);
+        }
+    })
+})
